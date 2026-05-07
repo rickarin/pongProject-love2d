652898143dc70d653f73ffe5fe90eabad6524b36
Ricardo Felipe <ricardofelipecrvg@gmail.com>
2026-05-06 22:41:30 -0300

    Algumas atualizações de qualidade

--- main.lua
+++ main.lua
@@ -25,20 +25,22 @@ function love.load()
 
     largeFont = love.graphics.newFont('Montserrat-Regular.ttf', 32)
     smallFont = love.graphics.newFont('Montserrat-Regular.ttf', 14)
-
+    fpsFont = love.graphics.newFont('Montserrat-Regular.ttf', 8)
 
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         resizable = false,
         vsync = true,
         fullscreen = false
     })
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH,WINDOW_HEIGHT, {upscale = 'normal'})
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        upscale = 'normal'
+    })
 end
 
 function love.update(dt)
     -- Jogador 1 Movimentação
     if love.keyboard.isDown('w') then
-        jogador1.dy = - VELOCIDADE
+        jogador1.dy = -VELOCIDADE
     elseif love.keyboard.isDown('s') then
         jogador1.dy = VELOCIDADE
     else
@@ -47,7 +49,7 @@ function love.update(dt)
 
     -- Jogador 2 Movimentação
     if love.keyboard.isDown('up') then
-        jogador2.dy = - VELOCIDADE
+        jogador2.dy = -VELOCIDADE
     elseif love.keyboard.isDown('down') then
         jogador2.dy = VELOCIDADE
     else
@@ -63,18 +65,16 @@ function love.update(dt)
     jogador2:update(dt)
 end
 
-
-
 function love.keypressed(key)
     -- Botão para fechar o jogo
     if key == 'escape' then
         love.event.quit()
 
-    -- Mudança de Estado do Jogo
+        -- Mudança de Estado do Jogo
     elseif key == 'enter' or key == 'return' then
         if estadodoJogo == 'Start' then
             estadodoJogo = 'Play'
-    else
+        else
             estadodoJogo = 'Start'
 
             bola:reset()
@@ -82,15 +82,12 @@ function love.keypressed(key)
     end
 end
 
-
-
 function love.draw()
     push:start()
 
     -- Fonte e Background
-    love.graphics.clear(52/255, 21/255, 57/255, 1)
+    love.graphics.clear(52 / 255, 21 / 255, 57 / 255, 1)
     love.graphics.setFont(smallFont)
-
 
     -- Jogadores
     jogador1Placar = 0
@@ -109,5 +106,16 @@ function love.draw()
     love.graphics.print(tostring(jogador1Placar), VIRTUAL_WIDTH / 2 - 45, 0)
     love.graphics.print(tostring(jogador2Placar), VIRTUAL_WIDTH / 2 + 25, 0)
 
+    displayFPS()
+
     push:finish()
 end
+
+-- Utilidades
+function displayFPS()
+    love.graphics.setFont(fpsFont)
+    love.graphics.setColor(0, 1, 0, 1)
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
+    love.graphics.setColor(1, 1, 1, 1)
+end
+
